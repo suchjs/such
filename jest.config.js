92aa475f0913f7a7425bd5f1ed18131251936713
ganmin <ganmin@360.cn>
2018-11-28 23:10:39 +0800

    remove spread operator from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
   testEnvironment: 'node',
-  transform: {
-    ...tsjPreset.transform
-  },
+  transform: Object.assign({},tsjPreset.transform),
   verbose: true,
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   testPathIgnorePatterns: ['<rootDir>/lib/','<rootDir>/dist/','<rootDir>/node_modules/','<rootDir>/src/'],
