244f19d4b8856d38912c3853df66c59a3ceacc20
ganmin <ganmin@360.cn>
2021-07-20 18:46:22 +0800

    fix: fix funcs parse negative number error, id support length

--- examples/index.ts
+++ examples/index.ts
@@ -6,18 +6,17 @@ import Such from '../src/index';
     const value = await Such.as({
       errno: ':number[0,1]:%d',
       errmsg: ':string{0,20}',
-      data: {
-        'count?': ':number[1e5,1e6]:%d',
-        'list{+0,5}': {
-          url: ':url',
-          date: ':date:%yyyy-mm-dd HH\\:MM\\:ss',
-          price: ':number[100,200]:%.2f',
-          content: ':regexp:/(?:[a-z]{3,8} ){3,10}/',
-          isNew: ':boolean',
-          color: ':color$rgba',
-        },
-        'from:{1}': ['a.com', 'b.com'],
+      'count?': ':number[1e5,1e6]:%d',
+      'list{+0,5}': {
+        id: ':id',
+        url: ':url',
+        date: ':date:%yyyy-mm-dd HH\\:MM\\:ss',
+        price: ':number[100,200]:%.2f',
+        content: ':regexp:/(?:[a-z]{3,8} ){3,10}/',
+        isNew: ':boolean',
+        color: ':color$rgba',
       },
+      'from:{1}': ['a.com', 'b.com'],
       'more{3}': '\\:number',
     });
     // eslint-disable-next-line no-console
--- src/mockit/id.ts
+++ src/mockit/id.ts
@@ -1,12 +1,15 @@
 import { TSuchInject } from '../types/instance';
 import Mockit from '../core/mockit';
-export default class ToId extends Mockit<number> {
+import { IPPSize } from '../types/parser';
+import { makeRandom } from 'src/helpers/utils';
+export default class ToId extends Mockit<number | number[]> {
   // set constructor name
   constructor(protected readonly constrName: string = 'ToId') {
     super(constrName);
   }
   // init
   public init(): void {
+    // set default config params
     this.configOptions = {
       step: {
         type: Number,
@@ -17,14 +20,33 @@ export default class ToId extends Mockit<number> {
         default: 1,
       },
     };
+    // allow $size, now it become to a range type
+    this.addRule('$size', function ($size: IPPSize) {
+      if (!$size) {
+        return;
+      }
+    });
   }
-  public generate(options: TSuchInject): number {
+  // generate
+  public generate(options: TSuchInject): number | number[] {
     const { dpath } = options;
-    const config = (this.params.$config || {}) as {
+    const { $config, $length } = this.params;
+    const config = ($config || {}) as {
       start?: number;
       step?: number;
     };
     const { start, step } = config;
+    if ($length) {
+      const { least, most } = $length;
+      const count = makeRandom(least, most);
+      let i = 0;
+      const result: number[] = [];
+      while (i < count) {
+        result.push(start + step * i);
+        i++;
+      }
+      return result;
+    }
     let len = dpath.length;
     while (len--) {
       const cur = dpath[len];
