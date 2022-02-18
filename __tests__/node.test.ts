fa029ff70e06c6f18a285b92843593ba7e2d43ce
fefit <jxz_211@163.com>
2022-02-18 23:51:06 +0800

    test: 💍 add more tests for node
    BREAKING CHANGE: 🧨 N
    
    ✅ Closes: N

--- __tests__/node.test.ts
+++ __tests__/node.test.ts
@@ -30,7 +30,7 @@ describe('validate built-in types in nodejs', () => {
   });
   // async
   test(':cascader after loadData', async () => {
-    await Such.loadData();
+    await Such.clearCache();
     const data = Such.as<{
       country: string
       state: string,
@@ -43,5 +43,23 @@ describe('validate built-in types in nodejs', () => {
     expect(typeof data.country === 'string').toBeTruthy();
     expect(typeof data.state === 'string').toBeTruthy();
     expect(typeof data.city === 'string').toBeTruthy();
+  });
+  // test async
+  test('asc method', async ()=> {
+    await Such.clearCache();
+    for(const _i of Array.from({
+      length: 100
+    })){
+      const data = await Such.asc<{
+        errno: number;
+        errmsg: string;
+        data?: {
+          province: string;
+        }
+      }>('mock.json');
+      expect([0, 1].includes(data.errno)).toBeTruthy();
+      expect(typeof data.errmsg === 'string').toBeTruthy();
+      expect(typeof data.data === undefined || typeof data.data.province === 'string').toBeTruthy();
+    }
   });
 });
